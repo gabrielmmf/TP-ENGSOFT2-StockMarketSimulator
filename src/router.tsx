9c7df2e430a8625a89dc6f9d65dc18a86b542312
Gabriel Torres Bolognani <gabrieltorres@dcc.ufmg.br>
2023-05-17 11:16:25 -0300

    init project

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import TradingOrderMatcher from './components/trading-order-matcher/trading-order-matcher'
+
+function Router () {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} key={'/'} element={<TradingOrderMatcher />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
